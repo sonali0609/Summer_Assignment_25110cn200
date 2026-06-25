206e0146847533899101c7b278b929fff9a05663
Sonali <sonali8368396313@gmail.com>
2026-06-25 16:33:37 +0530

    Day 22  programs

---
+++ Day 22/Q86.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[200];
+    int i, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+            words++;
+    }
+
+    printf("Total Words = %d", words);
+
+    return 0;
+}
